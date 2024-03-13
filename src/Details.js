29bdaed2ea401a194481e2f3e224e8ad9cebbf62
MustafaDadikhi <mnur.acik@gmail.com>
2024-03-13 21:10:56 +0300

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,87 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+    logogradient: logogradient,
+    logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+    name: "Müberra Nur açık",
+    tagline: "I build Mobil & Web Apps ",
+    img: profile,
+    about: `I'm Mübeera Nur Açık, a mobile app developer driven by a passion for crafting seamless user experiences. With a solid background in software engineering, I thrive on staying updated with the latest trends in the dynamic world of mobile applications. Let's connect and create something extraordinary together! `,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+    linkdein: "https://tr.linkedin.com/in/m%C3%BCberra-a%C3%A7ik-594374229",
+    github: "https://www.github.com/muberraacik",
+    twitter: "https://twitter.com/",
+    instagram: "https://www.instagram.com/muberra.acik",
+};
+
+// Enter your Work Experience here
+export const workDetails = [{
+        Position: "Software Engineering",
+        Company: `Hasan kalyoncu university`,
+        Location: "Türkiye / Gaziantep",
+        Type: "Student",
+        Duration: "2025 - 2025",
+    },
+
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+    html: html,
+    css: css,
+    js: js,
+    react: react,
+    npm: npm,
+    git: git,
+    github: github,
+    figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [];
+
+// Enter your Contact Details here
+export const contactDetails = {
+    email: "muberranuracik@gmail.com",
+    phone: "+90 551 632 9631",
+};
